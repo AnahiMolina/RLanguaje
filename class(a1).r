8b4f194e9b871668abe95ff33a55c5f5acb50d3a
alma <anahi_molina@outlook.com>
2024-09-27 12:10:47 -0600

    agregar archivo

---
+++ class(a1).r
@@ -0,0 +1,44 @@
+x<-NULL
+x
+##NULL
+x1<-c(1,2,3,4,5,6,7,8,9,10)
+x1
+##[1]1 2 3 4 5 6 7 8 9 10
+x2<-c(1:10)
+x2
+##
+
+a <- NULL
+a
+## NULL
+a1 <- ("a","b","c","d","e","f")
+a1
+##[1] "a","b","c","d","e","f"
+a2<-c("Hombre","Mujer","Hombre","Hombre","Mujer","Hombre","Mujer","Mujer")
+a2
+##[1] "Hombre","Mujer","Hombre","Hombre","Mujer","Hombre","Mujer","Mujer"
+##[9] "Hombre"
+
+#vector, elemento de una matriz.
+
+
+class(a1)
+## [1] "character"
+
+class(a2)
+## [1] "character"
+
+class(x1)
+## [1] "numeric"
+
+class(x2)
+## [1] "integer"
+
+class(x3)
+## [1] "numeric"
+
+class(f1)
+## [1] "factor"
+
+class(f1)
+## [1] "factor"
