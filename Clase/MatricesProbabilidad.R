2181815c8ebd0e8215771b4acee3ab505f3b252c
alma <anahi_molina@outlook.com>
2024-10-23 11:46:54 -0600

    archivo csv

--- Clase/MatricesProbabilidad.R
+++ Clase/MatricesProbabilidad.R
@@ -24,9 +24,53 @@ cuevas
 nabil
 
 
+#Tarea: buscar como unir dos dataframe
+
+rbind.data.frame()
+
+
+num_uno<-c(0:9)
+cartas_especiales<-c("+2","Reversa","Bloqueo") #son dos
+comodines<-c("+4","Cambio de color", "Comodin Minium") #son 4
+colores<-c("Rojo","Verde","Amarillo","Azul")
+
+
+combinatoria_cartas<-expand.grid(num=num_uno,color=colores)
+combinatoria_cartas
+
+combinatoria_especiales<-sample(expand.grid(ce=cartas_especiales,color=colores),2)
+combinatoria_especiales
+
+
+
+#combinatoria_especiales<-sample(paste(combinatoria_esp$color),2)
+
+
+T_comodines<-sample(paste(colores$cartas_especiales),2)
+
+
+
+
+
+
+
+
+
+
+
+
 #file.choose()
 getwd() #revisar la ruta actual
-setwd("C:/Users/Alma/OneDrive/Desktop/Gihub/RLanguaje") #cambiar la ruta
+#setwd("C:/Users/Alma/OneDrive/Desktop/Gihub/RLanguaje") #cambiar la ruta
+
+setwd("C:/Users/Alma/OneDrive/Desktop/Gihub/RLanguaje/Clase/bd")
+bd<-read.csv("nascar_driver_statistics.csv")
+
+
 
 write.csv(combinatoria, "tilin.csv")
 tilin2<-read.csv(file.choose(),sep=",")
+
+
+
+
